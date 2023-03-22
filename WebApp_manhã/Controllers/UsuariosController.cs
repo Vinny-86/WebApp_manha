92cb613aba9daab5bc94ab71a1bc524e625adf24
0220832211021 <0220832211021@LAB4MAQ15>
2023-03-22 11:00:00 -0300

    Add project files.

---
+++ WebApp_manhã/Controllers/UsuariosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_manhã.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+    }
+}

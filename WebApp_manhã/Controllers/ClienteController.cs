d96bc16adec034ab1e202f29c2d0677930009649
Vinny-86 <vinisilva.nh@gmail.com>
2023-03-29 11:02:32 -0300

    Atualização da Tela de Cadastro de Cliente, com demonstração do uso do BreakPoint

--- WebApp_manhã/Controllers/ClienteController.cs
+++ WebApp_manhã/Controllers/ClienteController.cs
@@ -8,5 +8,34 @@ namespace WebApp_manhã.Controllers
         {
             return View();
         }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+        {
+            if (string.IsNullOrEmpty(Nome)) {
+                TempData["Erro"] = "O campo Nome não pode estar vazio";
+            }
+
+            if (string.IsNullOrEmpty(Telefone)) {
+                TempData["Erro"] = "O campo Telefone não pode estar vazio";
+            }
+
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
     }
 }

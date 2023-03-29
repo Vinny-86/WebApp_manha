0ad4db549f4008b50cd883587ff10c770a926924
Vinny-86 <vinisilva.nh@gmail.com>
2023-03-29 09:09:18 -0300

    Implementação de lista de clientes com conceitos aplicados sobre o grid de 12 colunas do Bootstrap

---
+++ WebApp_manhã/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_manhã.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
